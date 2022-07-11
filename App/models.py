92799576c0830ebabf2a05c8fba48ae0316f8f86
Sakib Malik <maliksakib347@gmail.com>
2022-07-11 12:09:05 +0530

    Commit User first commit

---
+++ App/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+
+# Using Abstract User
+# class CustomUser(AbstractUser):
+#     username = None
+#     email = models.EmailField(_("Email Address"), unique=True)
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+#
+#     objects = CustomUserManager()
+#
+#     def __str__(self):
+#         return self.email
+#
+
+#     Using Base user class
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("Email Address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
